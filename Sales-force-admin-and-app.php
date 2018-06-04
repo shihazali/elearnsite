a7b247f0a1aab45492cb2d3fb5a88e06b49f4f1b
shihaz <shadab.arif@silvextech.com>
2018-06-04 11:14:23 +0530

    date format change

--- Sales-force-admin-and-app.php
+++ Sales-force-admin-and-app.php
@@ -238,7 +238,7 @@
 
 					<div class="row">
 
-						<div class="col-sm-7 col-md-8 col-lg-8">
+						<div class="col-sm-7 col-md-8 col-lg-7">
 
 							<article>
 								<div class="entry-thumbnail item-media rounded  bottommargin_40">
@@ -492,14 +492,14 @@
 						<!--eof .col-sm-8 (main content)-->
 
 						<!-- sidebar -->
-						<aside class="col-sm-5 col-md-4 col-lg-4">
+						<aside class="col-sm-5 col-md-4 col-lg-5">
 
 							<div class="widget widget_categories">
 
 								<h3 class="widget-title">Courses Date</h3>
 								<ol class="greylinks list3 text-uppercase greylinks bold">
 								
-									 <?php
+									  <?php
 										$processQuery = new ProcessQuery();
 										$rows = array();
 										$rows = $processQuery->selectWhere('course_table', 'C026');
@@ -513,12 +513,18 @@
                                             <div style=" border: 0.25px solid #29b5ee;">
                                             <div class="col-lg-4" style="margin-top: 8px !important; margin-bottom: 3px !important;">
         										<label class="content-justify">
-        											<?php echo $date; ?> to <?
+        											<?php
+													echo $date; ?>
+                            					</label> to 
+                            					</div>
+												<div class="col-lg-4" style="margin-top: 8px !important; margin-bottom: 3px !important;">
+        										<label class="content-justify">
+        											<?php
 													echo $edate; ?>
                             					</label>
                             					</div>
-                            					    <div class="col-lg-8" style="margin-top: 8px !important; margin-bottom: 3px !important;">
-                            					<a type="submit" id="enroll" name="enroll" class="theme_button color1" style="height: 30px; padding-top: 7px; color: #fff;" onclick="enrol(<?php echo $id; ?>);">Enroll Now</a>
+                            					    <div class="col-lg-4" style="margin-top: 8px !important; margin-bottom: 3px !important;">
+                            					<a type="submit" id="enroll" name="enroll" class="theme_button color1" style="height: 21px; padding-top: 3px; font-size: 11px; padding-left: 3px; padding-right: 3px; color: #fff;" onclick="enrol(<?php echo $id; ?>);">Enroll Now</a>
                             					</div>
                             					</div>
                             					<div class="clearfix"></div>
@@ -527,7 +533,6 @@
 									?>
 								</ol>
 							</div>
-
 						</aside>
 						<!-- eof aside sidebar -->
 
